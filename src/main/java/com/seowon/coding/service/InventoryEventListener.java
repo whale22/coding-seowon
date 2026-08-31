6697ed7d8e0b584fb4eeed0b1df33ccb5786eece
whale22 <realwhale64@naver.com>
2026-08-31 15:32:33 +0900

    feat: 이벤트 리스너 수정

--- src/main/java/com/seowon/coding/service/InventoryEventListener.java
+++ src/main/java/com/seowon/coding/service/InventoryEventListener.java
@@ -22,7 +22,7 @@ public class InventoryEventListener {
      */
 
     @TransactionalEventListener
-    public boolean handleOrderCreatedEvent(OrderCreatedEvent event) throws Exception {
+    public void handleOrderCreatedEvent(OrderCreatedEvent event) throws Exception {
         Exception RuntimeException = new RuntimeException();
         Product product;
         for(int i=0;i<event.getProductIds().size();i++){
